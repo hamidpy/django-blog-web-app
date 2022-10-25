cfa5ac850d742bb7667fd12f55ddc1b841dc4616
hamidpy <wdabdulhamid@gmail.com>
2022-10-25 22:53:26 +0600

    hide key

--- Blog/settings.py
+++ Blog/settings.py
@@ -10,11 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p8*q920vzq=$91%zfcc)=i0%xr99k7$!gw^f^v8b6n19qhuf$j'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['katenblog.herokuapp.com']
 
