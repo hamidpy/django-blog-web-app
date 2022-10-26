af2e1ccd4a139da4a5d5b4199c887a0c7a9c6eb4
hamidpy <wdabdulhamid@gmail.com>
2022-10-26 13:51:29 +0600

    Imap

--- Blog/settings.py
+++ Blog/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from decouple import config
 
 
-SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+SECRET_KEY = config('SECRET_KEY') # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
